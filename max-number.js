9dbf1f1f398e1d1d2d2f60af792996f3eace2f87
hellosiyam <hellosiyambd@gmail.com>
2025-01-06 14:28:17 +0600

    mouduile 22

--- max-number.js
+++ max-number.js
@@ -1,8 +1,24 @@
-const number = [14, 25, 45, 63, 87, 102];
-let lrg = 0;
-for(const lrgNumber of number){
-    if (lrgNumber > lrg  ) {
-        lrg = lrgNumber
+// const number = [14, 25, 45, 63, 87, 102];
+// let lrg = 0;
+// for(const lrgNumber of number){
+//     if (lrgNumber > lrg  ) {
+//         lrg = lrgNumber
+//     }
+// }
+// console.log(lrg);
+
+const numbers = [14, 25, 63, 78, 45, 12,58];
+
+function getMex(heighest) {
+    let num = numbers[0]
+    for(const number of heighest)
+    if (number < num) {
+        num = number
     }
+    return num
+    
 }
-console.log(lrg);
+const result = getMex(numbers);
+console.log(result);
+
+
--- price.js
+++ price.js
@@ -1,19 +1,40 @@
-const mobiles = [
-    {phone : "Apple", price : 27000 , color : "Red", camera : "12mp"},
-    {phone : "SamSung", price : 120000 , color : "Red", camera : "12mp"},
-    {phone : "Mi", price : 45000 , color : "Red", camera : "12mp"},
-    {phone : "Realmi", price : 60000 , color : "Red", camera : "12mp"},
-    {phone : "Asus", price : 20000 , color : "Red", camera : "12mp"}
-]
-function mob(params) {
-    let prices = params[0]  
-    for(const pera of params){
-        if (pera.price < prices.price) {
-            prices = pera
-        }
-    }
-    return prices
-}
+// const mobiles = [
+//     {phone : "Apple", price : 27000 , color : "Red", camera : "12mp"},
+//     {phone : "SamSung", price : 120000 , color : "Red", camera : "12mp"},
+//     {phone : "Mi", price : 45000 , color : "Red", camera : "12mp"},
+//     {phone : "Realmi", price : 60000 , color : "Red", camera : "12mp"},
+//     {phone : "Asus", price : 20000 , color : "Red", camera : "12mp"}
+// ]
+// function mob(params) {
+//     let prices = params[0]  
+//     for(const pera of params){
+//         if (pera.price < prices.price) {
+//             prices = pera
+//         }
+//     }
+//     return prices
+// }
 
-const result = mob(mobiles)
+// const result = mob(mobiles)
+// console.log(result);
+
+const mobiles = [
+    {phone : "Apple", price : 140000, color : "red", camera : "16mp"},
+    {phone : "samsung", price : 92000, color : "red", camera : "16mp"},
+    {phone : "pixel", price : 78000, color : "red", camera : "16mp"},
+    {phone : "Rog", price : 168000, color : "red", camera : "16mp"},
+    {phone : "Huwai", price : 88000, color : "red", camera : "16mp"}
+]
+
+function mobile(ctg) {
+    let mob = ctg[0]
+   for(const all of ctg){
+    if (all.price < mob.price) {
+       mob = all
+    }
+   }
+   return mob;
+}
+const result = mobile(mobiles);
 console.log(result);
+
